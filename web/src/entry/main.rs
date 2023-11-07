a1956055ceffc239fe9499698710fe87019c9450
Diaan Engelbrecht <diaan.engelbrecht@gmail.com>
2023-11-07 23:26:59 +0100

    feat: experiment #1 with server config

--- web/src/entry/main.rs
+++ web/src/entry/main.rs
@@ -1,3 +1,4 @@
+use cali_core::config::CaliConfig;
 use cali_derive::setup_server;
 use lightweight_web::config::Config;
 use std::{error::Error, str::FromStr, sync::Arc};
@@ -7,7 +8,7 @@ pub struct ServerContext {}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let my_context = Arc::new(ServerContext {});
-    setup_server!("lightweight", "0.1.1", my_context);
+    let server_config = CaliConfig::new(Arc::new(ServerContext {}), true, |s| s);
+    setup_server!("lightweight", "0.1.1", server_config);
     Ok(())
 }
